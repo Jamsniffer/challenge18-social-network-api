84f5eb7797f4d03ff9d1cdd69a683a5e5197e01c
Royal Jam <103871471+Jamsniffer@users.noreply.github.com>
2022-10-17 23:51:02 -0700

    routes for users and thoughts finished

---
+++ controllers/thought-controller.js
@@ -0,0 +1,70 @@
+const { Thought } = require('../models');
+
+const ThoughtController = {
+    //get all Thoughts
+
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    
+    //get one Thought by id
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({_id: params.id })
+        .then(dbThoughtData => {
+            // if no Thought is found, send 404
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No Thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    //create Thought
+
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    //update a Thought
+
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true})
+        .then(dbThoughtData => {
+            if(!dbThoughtData){
+                res.status(404).json({ message: 'No Thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    //delete a Thought
+
+    deleteThought({ params}, res) {
+        Thought.findOneAndDelete({ _id: params.id})
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({ message: 'No Thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+}
+
+module.export = ThoughtController
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
 
 const {
-    // getAllPizza,
-    // getPizzaById,
-    // createPizza,
-    // updatePizza,
-    // deletePizza
-} = require('../../controllers/pizza-controller');
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
 
 // Set up GET all and POST at /api/pizzas
 
 router
     .route('/')
-    .get(getAllPizza)
-    .post(createPizza);
+    .get(getAllUsers)
+    .post(createUser);
 
     //set up GET one, PUT, and DELETE at /api/pizzas/:id
 
 router
     .route('/:id')
-    .get(getPizzaById)
-    .put(updatePizza)
-    .delete(deletePizza);
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 
 
